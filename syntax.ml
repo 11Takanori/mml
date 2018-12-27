dc8090415cc33f4c55a3a3a7f3ab0491c5a9a383
Takanori Ishibashi <takanori.1112@gmail.com>
2018-12-27 11:32:52 +0900

    Add syntax

---
+++ syntax.ml
@@ -0,0 +1,13 @@
+type id = string
+
+type binOp = Plus | Mult | Lt
+
+type exp =
+    Var of id
+  | ILit of int
+  | BLit of bool
+  | BinOp of binOp * exp * exp
+  | IfExp of exp * exp * exp
+
+type program = 
+    Exp of exp
