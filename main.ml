6d48053c49653d3fd4df50f2460acd752f4146b1
Takanori Ishibashi <takanori.1112@gmail.com>
2018-12-28 21:55:32 +0900

    Add error handling in main

--- main.ml
+++ main.ml
@@ -3,12 +3,20 @@ open Eval
 let rec read_eval_print env =
   print_string "# ";
   flush stdout;
-  let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
-  let (id, newenv, v) = eval_decl env decl in
-    Printf.printf "val %s = " id;
-    pp_val v;
+  let showError str = Printf.printf "%s" str;
     print_newline();
-    read_eval_print newenv
+    read_eval_print env in
+  (try
+      let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+      let (id, newenv, v) = eval_decl env decl in
+        Printf.printf "val %s = " id;
+        pp_val v;
+        print_newline();
+        read_eval_print newenv
+      with Failure str -> showError str
+      | Eval.Error str -> showError str
+      | Parsing.Parse_error -> showError "parse error"
+      | _ -> showError "Other Execption")
 
 let initial_env = 
   Environment.extend "i" (IntV 1)
