8f2511c93c8d405e7a7316bf45b9f9c526128e7f
Takanori Ishibashi <takanori.1112@gmail.com>
2019-01-06 11:08:53 +0900

    Fix ty_exp

--- src/typing.ml
+++ src/typing.ml
@@ -8,67 +8,7 @@ type tyenv = ty Environment.t
 
 type subst = (tyvar * ty) list
 
-let ty_prim op ty1 ty2 =
-  match op with
-  | Plus -> (
-    match (ty1, ty2) with
-    | TyInt, TyInt -> TyInt
-    | _ -> err "Argument must be integer: +" )
-  | Mult -> (
-    match (ty1, ty2) with
-    | TyInt, TyInt -> TyInt
-    | _ -> err "Argument must be integer: *" )
-  | Lt -> (
-    match (ty1, ty2) with
-    | TyInt, TyInt -> TyBool
-    | _ -> err "Argument must be integer: <" )
-  | And -> (
-    match (ty1, ty2) with
-    | TyBool, TyBool -> TyBool
-    | _ -> err "Argument must be bool: &&" )
-  | Or -> (
-    match (ty1, ty2) with
-    | TyBool, TyBool -> TyBool
-    | _ -> err "Argument must be bool: ||" )
-
-let ty_prim2 op ty1 ty2 = match op with
-  | Plus -> ([(ty1, TyInt); (ty2, TyInt)], TyInt)
-  | Mult -> ([(ty1, TyInt); (ty2, TyInt)], TyInt)
-  | Lt -> ([(ty1, TyInt); (ty2, TyInt)], TyBool)
-  | And -> ([(ty1, TyInt); (ty2, TyInt)], TyBool)
-  | Or -> ([(ty1, TyInt); (ty2, TyInt)], TyBool)
-
-let rec ty_exp tyenv = function
-  | Var x -> (
-    try Environment.lookup x tyenv with Environment.Not_bound ->
-      err ("Variable not bound: " ^ x) )
-  | ILit _ -> TyInt
-  | BLit _ -> TyBool
-  | BinOp (op, exp1, exp2) ->
-      let tyarg1 = ty_exp tyenv exp1 in
-      let tyarg2 = ty_exp tyenv exp2 in
-      ty_prim op tyarg1 tyarg2
-  | IfExp (exp1, exp2, exp3) -> (
-      let tycond = ty_exp tyenv exp1 in
-      let tythen = ty_exp tyenv exp2 in
-      let tyelse = ty_exp tyenv exp3 in
-      match tycond with
-      | TyBool ->
-          if tythen = tyelse then tythen
-          else
-            err
-              "Type of then expression and that of else expression must be \
-               same type"
-      | _ -> err "Conditon must be bool" )
-  | LetExp (id, exp1, exp2) ->
-      let tyvalue = ty_exp tyenv exp1 in
-      ty_exp (Environment.extend id tyvalue tyenv) exp2
-  | _ -> err "Not Implemented"
-
-let ty_decl tyenv = function
-  | Exp e -> ty_exp tyenv e
-  | Decl (_, e) -> ty_exp tyenv e
-  | _ -> err "Not Implemented"
+let eqs_of_subst s = []
 
 let subst_type s typ =
   let rec resolve_type s = function
@@ -99,3 +39,49 @@ let rec unify = function
         if MySet.member var (Syntax.freevar_ty ty1) then err "Type err"
         else unify rest @ [(var, ty1)]
     | _, _ -> unify rest )
+
+let ty_prim op ty1 ty2 = match op with
+  | Plus -> ([(ty1, TyInt); (ty2, TyInt)], TyInt)
+  | Mult -> ([(ty1, TyInt); (ty2, TyInt)], TyInt)
+  | Lt -> ([(ty1, TyInt); (ty2, TyInt)], TyBool)
+  | And -> ([(ty1, TyInt); (ty2, TyInt)], TyBool)
+  | Or -> ([(ty1, TyInt); (ty2, TyInt)], TyBool)
+
+let rec ty_exp tyenv = function
+  | Var x -> (
+    try [], Environment.lookup x tyenv with Environment.Not_bound ->
+      err ("Variable not bound: " ^ x) )
+  | ILit _ -> [], TyInt
+  | BLit _ -> [], TyBool
+  | BinOp (op, exp1, exp2) ->
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s2, ty2) = ty_exp tyenv exp2 in
+      let (eqs3, ty) = ty_prim op ty1 ty2 in
+      let eqs = (eqs_of_subst s1) @ (eqs_of_subst s2) @ eqs3 in
+      let s3 = unify eqs in (s3, subst_type s3 ty)
+  | IfExp (exp1, exp2, exp3) ->
+      let (scond, tycond) = ty_exp tyenv exp1 in
+      let (sthen, tythen) = ty_exp tyenv exp2 in
+      let (selse, tyelse) = ty_exp tyenv exp3 in
+      let eqs3 = [(tycond, TyBool); (tythen, tyelse)] in
+      let eqs = (eqs_of_subst scond) @ (eqs_of_subst sthen) @ (eqs_of_subst selse) @ eqs3 in
+      let s3 = unify eqs in (s3, subst_type s3 tythen)
+  | LetExp (id, exp1, exp2) ->
+      let (s1, ty1) = ty_exp tyenv exp1 in
+      let (s2, ty2) = ty_exp (Environment.extend id ty1 tyenv) exp2 in
+      let domty = TyVar (fresh_tyvar ()) in
+      let eqs3 = [(domty, ty1)] in
+      let eqs = (eqs_of_subst s1) @ eqs3 @ (eqs_of_subst s2) in
+      let s3 = unify eqs in (s3, subst_type s3 ty2)
+  | FunExp (id, exp) ->
+      let domty = TyVar (fresh_tyvar ()) in
+      let s, ranty =
+        ty_exp (Environment.extend id domty tyenv) exp in
+        (s, TyFun (subst_type s domty, ranty))
+  | AppExp (exp1, exp2) -> ([], TyInt)
+  | _ -> err "Not Implemented"
+
+let ty_decl tyenv = function
+  | Exp e -> ty_exp tyenv e
+  | Decl (_, e) -> ty_exp tyenv e
+  | _ -> err "Not Implemented"
